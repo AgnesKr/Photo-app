f798c352af9be1a0ca985a2ebd55ac73a7d01c7b
AgnesKr <weltreise@wp.pl>
2019-07-12 17:37:22 +0200

    v1

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const column1Element = document.querySelector(".column-1"),
+    column2Element = document.querySelector(".column-2"),
+    inputElement = document.querySelector("input"),
+    photosElements = document.querySelector(".photos")
+
 let page = 1,
     nextPage = null,
     isWaiting = false
@@ -14,21 +19,21 @@ axios({
             openImage(response.data[i].urls.regular)
         })
         if (i % 2 == 0) {
-            document.querySelector(".column-1").append(imgElement)
+            column1Element.append(imgElement)
         } else {
-            document.querySelector(".column-2").append(imgElement)
+            column2Element.append(imgElement)
         }
     }
-    nextPage = document.querySelector(".photos").getBoundingClientRect().height + document.querySelector(".photos").getBoundingClientRect(".photos").top - 100
+    nextPage = photosElements.getBoundingClientRect().height + photosElements.getBoundingClientRect().top - 100
 })
 
-document.querySelector("input").addEventListener("input", function () {
+inputElement.addEventListener("input", function () {
     isWaiting = true
     axios({
         url: `https://api.unsplash.com/search/photos?page=1&query=${this.value}&client_id=01b0f4bc8fd2366b73face2e6e307f3fa3e51f23bc492ce26448155cc3d54602`
     }).then((response) => {
-        document.querySelector(".column-1").innerHTML = ""
-        document.querySelector(".column-2").innerHTML = ""
+        column1Element.innerHTML = ""
+        column2Element.innerHTML = ""
         for (let i = 0; i < response.data.results.length; i++) {
             let imgElement = document.createElement("img")
             imgElement.setAttribute("src", response.data.results[i].urls.regular)
@@ -36,13 +41,13 @@ document.querySelector("input").addEventListener("input", function () {
                 openImage(response.data.results[i].urls.regular)
             })
             if (i % 2 == 0) {
-                document.querySelector(".column-1").append(imgElement)
+                column1Element.append(imgElement)
             } else {
-                document.querySelector(".column-2").append(imgElement)
+                column2Element.append(imgElement)
             }
         }
         page = 1
-        nextPage = document.querySelector(".photos").getBoundingClientRect().height + document.querySelector(".photos").getBoundingClientRect(".photos").top - 100
+        nextPage = photosElements.getBoundingClientRect().height + photosElements.getBoundingClientRect().top - 100
         isWaiting = false
     })
 })
@@ -64,7 +69,7 @@ window.addEventListener("scroll", function () {
     if (nextPage < window.pageYOffset + window.innerHeight && !isWaiting) {
         isWaiting = true
         page++
-        if(document.querySelector("input").value === "") {
+        if (inputElement.value === "") {
             axios({
                 method: "GET",
                 url: `https://api.unsplash.com/photos?page=${page}&client_id=01b0f4bc8fd2366b73face2e6e307f3fa3e51f23bc492ce26448155cc3d54602`
@@ -76,18 +81,17 @@ window.addEventListener("scroll", function () {
                         openImage(response.data[i].urls.regular)
                     })
                     if (i % 2 == 0) {
-                        document.querySelector(".column-1").append(imgElement)
+                        column1Element.append(imgElement)
                     } else {
-                        document.querySelector(".column-2").append(imgElement)
+                        column2Element.append(imgElement)
                     }
                 }
-                nextPage = document.querySelector(".photos").getBoundingClientRect().height + document.querySelector(".photos").getBoundingClientRect(".photos").top - 100
+                nextPage = photosElements.getBoundingClientRect().height + photosElements.getBoundingClientRect().top - 100
                 isWaiting = false
             })
         } else {
-            console.log("stronka")
             axios({
-                url: `https://api.unsplash.com/search/photos?page=${page}&query=${document.querySelector("input").value}&client_id=01b0f4bc8fd2366b73face2e6e307f3fa3e51f23bc492ce26448155cc3d54602`
+                url: `https://api.unsplash.com/search/photos?page=${page}&query=${inputElement.value}&client_id=01b0f4bc8fd2366b73face2e6e307f3fa3e51f23bc492ce26448155cc3d54602`
             }).then((response) => {
                 for (let i = 0; i < response.data.results.length; i++) {
                     let imgElement = document.createElement("img")
@@ -96,15 +100,15 @@ window.addEventListener("scroll", function () {
                         openImage(response.data.results[i].urls.regular)
                     })
                     if (i % 2 == 0) {
-                        document.querySelector(".column-1").append(imgElement)
+                        column1Element.append(imgElement)
                     } else {
-                        document.querySelector(".column-2").append(imgElement)
+                        column2Element.append(imgElement)
                     }
                 }
-                nextPage = document.querySelector(".photos").getBoundingClientRect().height + document.querySelector(".photos").getBoundingClientRect(".photos").top - 100
+                nextPage = photosElements.getBoundingClientRect().height + photosElements.getBoundingClientRect().top - 100
                 isWaiting = false
             })
         }
-       
+
     }
 })
